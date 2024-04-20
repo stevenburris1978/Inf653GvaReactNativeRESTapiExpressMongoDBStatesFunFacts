d61da60ace852ed6bb53e16a1cf8734e45c7cbbe
skylor burris <s_burris@mail.fhsu.edu>
2024-04-19 20:47:28 -0500

    kayscrochetmobile app 1.0

--- kayscrochet-backend/server.js
+++ kayscrochet-backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/dbConfig');
+const connectDB = require('./config/dbConfig.js');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
